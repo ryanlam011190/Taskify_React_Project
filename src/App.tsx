7f7c6a3e897184cdf7e37b72263996ed6a3b7b59
KhoaLam <ryan.lam1990@gmail.com>
2022-09-10 21:13:14 -0700

    first push

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import InputField from './Components/InputField';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <span className="heading"> Taskify</span>
+      <InputField></InputField>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/InputField.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './styles.css'
+const InputField = () => {
+  return (
+    <form className='input'>
+        <input type='input' placeholder='Enter a task' className='input_box'></input>
+        <button className='input_submit' type="submit"> Go </button>
+    </form> 
+  )
+}
+
+export default InputField
